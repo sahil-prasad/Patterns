2c004e4ddb6448fcf149653c868b7892a766d2d3
Sahil <05sahilprasad@gmail.com>
2023-11-09 16:38:42 +0530

    Add

---
+++ halfupperpyramid.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class halfupperpyramid
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Enter the order");
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c;
+        c=r;
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(i<=j)
+                {
+                    System.out.print(" *");
+                }
+                else
+                {
+                    System.out.print(" ");
+                    
+                }System.out.println();
+            }
+        }
+
+    }
+}
