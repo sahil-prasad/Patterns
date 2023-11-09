946eb85998eb781e7c9926df277d99404e0ab200
Sahil <05sahilprasad@gmail.com>
2023-11-09 16:46:09 +0530

    Add

--- halflowerpyramid.java
+++ halflowerpyramid.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class halfupperpyramid
+public class halflowerpyramid
 {
     public static void main(String[]args)
     {
@@ -12,7 +12,7 @@ public class halfupperpyramid
         {
             for(int j=1;j<=c;j++)
             {
-                if(i<=j)
+                if(i>=j)
                 {
                     System.out.print(" *");
                 }
